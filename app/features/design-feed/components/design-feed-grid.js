6785b8eb831db6b74d205194cae3422637b299dd
ghazi-mansoor <mansoor.ghazi@gmail.com>
2023-06-02 12:10:58 -0300

    Add id key

--- app/features/design-feed/components/design-feed-grid.js
+++ app/features/design-feed/components/design-feed-grid.js
@@ -2,6 +2,7 @@ import {ChatBubbleLeftIcon, EyeIcon, HeartIcon} from '@heroicons/react/20/solid'
 
 const files = [
     {
+        id: 1,
         title: 'File title',
         designer: 'John Doe',
         comments: 0,
@@ -11,6 +12,7 @@ const files = [
             'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
     },
     {
+        id: 2,
         title: 'File title',
         designer: 'John Doe',
         comments: 0,
@@ -20,6 +22,7 @@ const files = [
             'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
     },
     {
+        id: 3,
         title: 'File title',
         designer: 'John Doe',
         comments: 0,
@@ -29,6 +32,7 @@ const files = [
             'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
     },
     {
+        id: 4,
         title: 'File title',
         designer: 'John Doe',
         comments: 0,
@@ -38,6 +42,7 @@ const files = [
             'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
     },
     {
+        id: 5,
         title: 'File title',
         designer: 'John Doe',
         comments: 0,
@@ -47,6 +52,7 @@ const files = [
             'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
     },
     {
+        id: 6,
         title: 'File title',
         designer: 'John Doe',
         comments: 0,
@@ -62,7 +68,7 @@ export default function DesignFeedGrid() {
     return (
         <ul role="list" className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 xl:gap-x-8">
             {files.map((file) => (
-                <li key={file.source} className="relative">
+                <li key={file.id} className="relative">
                     <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
                         <img src={file.source} alt="" className="pointer-events-none object-cover group-hover:opacity-75" />
                         <button type="button" className="absolute inset-0 focus:outline-none">
