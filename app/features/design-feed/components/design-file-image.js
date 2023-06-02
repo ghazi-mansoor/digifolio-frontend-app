ec998ef5a5dd5703c22e0e4a420ebf012c42437b
ghazi-mansoor <mansoor.ghazi@gmail.com>
2023-06-02 14:08:42 -0300

    add rounded corners

--- app/features/design-feed/components/design-file-image.js
+++ app/features/design-feed/components/design-file-image.js
@@ -1,6 +1,6 @@
 export default function DesignFileImage({ fileURL, fileTitle }) {
     return (
-        <div className="flex justify-center bg-slate-50">
+        <div className="flex justify-center bg-slate-50 rounded-md">
             <img className="object-fit" src="/app_design_image.png" alt={fileTitle} />
         </div>
     );
