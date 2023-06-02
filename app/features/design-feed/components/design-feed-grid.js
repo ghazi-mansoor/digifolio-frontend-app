061c80377f84ce68a8f821dc3566a3d7511801ae
ghazi-mansoor <mansoor.ghazi@gmail.com>
2023-06-02 11:58:37 -0300

    Display designs in grid

---
+++ app/features/design-feed/components/design-feed-grid.js
@@ -0,0 +1,89 @@
+const files = [
+    {
+        title: 'File title',
+        designer: 'John Doe',
+        comments: 0,
+        likes: 2,
+        views: '1.1k',
+        source:
+            'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+    },
+    {
+        title: 'File title',
+        designer: 'John Doe',
+        comments: 0,
+        likes: 2,
+        views: '1.1k',
+        source:
+            'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+    },
+    {
+        title: 'File title',
+        designer: 'John Doe',
+        comments: 0,
+        likes: 2,
+        views: '1.1k',
+        source:
+            'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+    },
+    {
+        title: 'File title',
+        designer: 'John Doe',
+        comments: 0,
+        likes: 2,
+        views: '1.1k',
+        source:
+            'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+    },
+    {
+        title: 'File title',
+        designer: 'John Doe',
+        comments: 0,
+        likes: 2,
+        views: '1.1k',
+        source:
+            'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+    },
+    {
+        title: 'File title',
+        designer: 'John Doe',
+        comments: 0,
+        likes: 2,
+        views: '1.1k',
+        source:
+            'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
+    },
+    // More files...
+]
+
+export default function DesignFeedGrid() {
+    return (
+        <ul role="list" className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 xl:gap-x-8">
+            {files.map((file) => (
+                <li key={file.source} className="relative">
+                    <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+                        <img src={file.source} alt="" className="pointer-events-none object-cover group-hover:opacity-75" />
+                        <button type="button" className="absolute inset-0 focus:outline-none">
+                            <span className="sr-only">View details for {file.title}</span>
+                        </button>
+                    </div>
+                    <div className="flex w-full flex-row items-center justify-between">
+                        <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">{file.designer}</p>
+                        <div className="flex flex-row space-x-2">
+                            <p>
+
+                                <span className="pointer-events-none mt-2 block text-sm font-medium text-gray-500">{file.comments}</span>
+                            </p>
+                            <p>
+                                <span className="pointer-events-none mt-2 block text-sm font-medium text-gray-500">{file.likes}</span>
+                            </p>
+                            <p>
+                                <span className="pointer-events-none mt-2 block text-sm font-medium text-gray-500">{file.views}</span>
+                            </p>
+                        </div>
+                    </div>
+                </li>
+            ))}
+        </ul>
+    )
+}
--- app/features/design-feed/components/design-feed.jsx
+++ app/features/design-feed/components/design-feed.jsx
@@ -1,14 +1,18 @@
 import SignedInNavbar from "@/app/shared/components/navigation/signed-in-navbar";
 import Footer from "@/app/shared/components/layout/footer";
 import DesignFeedHeader from "@/app/features/design-feed/components/design-feed-header";
+import DesignFeedGrid from "@/app/features/design-feed/components/design-feed-grid";
 
 
 export default function DesignFeed() {
     return (
-        <div className="flex flex-col w-full min-h-full space-y-20">
+        <div className="flex flex-col w-full min-h-full space-y-14">
             <SignedInNavbar />
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 w-full">
                 <DesignFeedHeader />
+            </div>
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 w-full">
+                <DesignFeedGrid />
             </div>
             <Footer />
         </div>
