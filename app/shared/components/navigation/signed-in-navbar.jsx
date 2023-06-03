8eb2a8f902f030c978c60f389ffcea3ceac8f4a2
ghazi-mansoor <mansoor.ghazi@gmail.com>
2023-06-03 11:47:21 -0300

    add link to file upload page

--- app/shared/components/navigation/signed-in-navbar.jsx
+++ app/shared/components/navigation/signed-in-navbar.jsx
@@ -127,7 +127,7 @@ export default function SignedInNavbar() {
                                     </Menu>
 
                                     <a
-                                        href="#"
+                                        href="/upload-file"
                                         className="ml-6 inline-flex items-center rounded-md bg-pink-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600"
                                     >
                                         <PlusIcon className="-ml-0.5 mr-0.5 h-5 w-5" aria-hidden="true" />
