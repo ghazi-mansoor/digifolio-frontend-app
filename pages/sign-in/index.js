83dc34976bf21b8121237b3c95d856af46c9168d
ghazi-mansoor <mansoor.ghazi@gmail.com>
2023-06-03 22:29:00 -0300

    Add sign in page

---
+++ pages/sign-in/index.js
@@ -0,0 +1,9 @@
+import SignIn from "@/app/features/sign-in/components/sign-in";
+
+export default function SignInPage() {
+    return (
+      <div className="h-screen w-screen">
+          <SignIn />
+      </div>
+    );
+}
